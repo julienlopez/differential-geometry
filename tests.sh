34f3080ea0fc947983807bf984532b342c467313
julienlopez <julien.lopez51@gmail.com>
2015-02-05 11:35:36 +0100

    trying to get code coverage on travis-ci

--- tests.sh
+++ tests.sh
@@ -7,21 +7,11 @@ function error_exit
 	exit 1
 }
 
-echo "resetting coverage values"
-lcov --zerocounters $DIR_OPTIONS #> lcov.log 2> lcov.err
-
-echo "running baseline"
-lcov --capture --initial $DIR_OPTIONS --output-file app.baseline #>> lcov.log 2>> lcov.err
-
 bin/tests
 [ $? -ne 0 ] && exit 1
 
-echo "analyzing coverage data...: lcov --no-checksum $DIR_OPTIONS --capture --output-file app"
-lcov --no-checksum $DIR_OPTIONS --capture --output-file app #>> lcov.log 2>> lcov.err
-#lcov --no-checksum $DIR_OPTIONS --no-external --capture --output-file app.info
-
-echo "merging datas"
-lcov --add-tracefile app.baseline --add-tracefile app --output-file app.info
+echo "analyzing coverage data...: lcov $DIR_OPTIONS --capture --output-file app.info"
+lcov $DIR_OPTIONS --capture --output-file app.info
 
 echo "removing useless data"
 lcov --remove app.info "/usr*" --output-file app.info
